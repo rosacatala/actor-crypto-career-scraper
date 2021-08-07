557097b6d9957d9fd19c6155b4004952e6a6c16d
Rosa C <rcatalaluque@gmail.com>
2021-08-07 08:51:48 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ Apify.main(async () => {
     // Function called for each URL
     const handlePageFunction = async ({ request, $ }) => {
         console.log(request.url);
+        const { body } = await Apify.utils.requestAsBrowser();
         // Add some links from page to RequestQueue
         await Apify.utils.enqueueLinks({
             $,
