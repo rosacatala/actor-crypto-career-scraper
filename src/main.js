e771522fd64c3b53c3b2ccbe769302990c07c803
Rosa C <rcatalaluque@gmail.com>
2021-08-06 18:48:01 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,19 +4,18 @@ Apify.main(async () => {
     // Create a RequestQueue
     const requestQueue = await Apify.openRequestQueue();
     // Define the starting URL
-    await requestQueue.addRequest({ url: 'https://www.crypto-careers.com/' });
+    await requestQueue.addRequest({ url: 'https://apify.com/' });
     // Function called for each URL
-    const handlePageFunction = async ({ request, $ }) => {
+    const handlePageFunction = async ({ request, page }) => {
         console.log(request.url);
         // Add all links from page to RequestQueue
         await Apify.utils.enqueueLinks({
-            $,
+            page,
             requestQueue,
-            baseUrl: request.loadedUrl,
         });
     };
-    // Create a CheerioCrawler
-    const crawler = new Apify.CheerioCrawler({
+    // Create a PuppeteerCrawler
+    const crawler = new Apify.PuppeteerCrawler({
         requestQueue,
         handlePageFunction,
         maxRequestsPerCrawl: 10, // Limitation for only 10 requests (do not use if you want to crawl all links)
